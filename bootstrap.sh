9221ccfd7511a48055097e68ac5b18cfcdd7d35c
chai2010 <chaishushan@gmail.com>
2017-12-23 00:28:08 +0800

    zz

--- bootstrap.sh
+++ bootstrap.sh
@@ -38,5 +38,8 @@ EOF
 # restart docker
 sudo systemctl restart docker
 
+# install docker-compose
+# sudo curl -L https://github.com/docker/compose/releases/download/1.18.0/docker-compose-`uname -s`-`uname -m` -o /usr/local/bin/docker-compose
+
 # run app in docker
 # docker run hello-world
