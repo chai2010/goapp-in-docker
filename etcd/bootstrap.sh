db5f1734f5fc06abbeb9540ff81c63200fdea68a
chai2010 <chaishushan@gmail.com>
2017-12-23 16:15:14 +0800

    zz

---
+++ etcd/bootstrap.sh
@@ -0,0 +1,9 @@
+#!/bin/sh
+# Copyright 2017 <chaishushan{AT}gmail.com>. All rights reserved.
+# Use of this source code is governed by a BSD license
+# that can be found in the LICENSE file.
+
+uname -a
+
+# add to root group
+sudo usermod -G root vagrant
